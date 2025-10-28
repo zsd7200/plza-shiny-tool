3ebd258318e27075ffc2cfeba4a839760051aa28
Zack Dunham <dunhamz2016@gmail.com>
2025-10-28 17:06:49 -0400

    change loop to switch statement

--- plza-shiny-tool.ino
+++ plza-shiny-tool.ino
@@ -1,12 +1,16 @@
-#include "LUFAConfig.h"
 #include <LUFA.h>
-#include "Joystick.h"
-#include "General.h"
-#include "WildZone.h"
-#include "SimpleScripting.h"
+#include "src/LUFAConfig.h"
+#include "src/Joystick.h"
+#include "src/General.h"
+#include "src/WildZone.h"
+#include "src/SimpleScripting.h"
 
 Simple simple;
 WildZone wildZone;
+
+// set desired wild zone number / area ID here
+// or set to 999 for simpleScript
+int zoneId = -1;
 
 void setup() {
   SetupHardware(); // Needed for LUFA
@@ -16,30 +20,73 @@ void setup() {
 }
 
 void loop() {
-  // uncomment your desired Wild Zone
-  //wildzone.One();
-  //wildzone.Two();
-  //wildzone.Three();
-  //wildzone.Four();
-  //wildzone.Five();
-  //wildzone.Six();
-  //wildzone.Seven();
-  //wildzone.Eight();
-  //wildzone.Nine();
-  //wildzone.Ten();
-  //wildzone.Eleven();
-  //wildzone.Twelve();
-  //wildzone.Thirteen();
-  //wildzone.Fourteen();
-  //wildzone.Fifteen();
-  //wildzone.Sixteen();
-  //wildzone.Seventeen();
-  //wildZone.Eighteen();
-  //wildZone.Nineteen();
-  //wildZone.Twenty();
-
-  // OR...
-  // simpleScript();
+  switch (zoneId) {
+    case 1:
+      wildZone.One();
+      break;
+    case 2:
+      wildZone.Two();
+      break;
+    case 3:
+      wildZone.Three();
+      break;
+    case 4:
+      wildZone.Four();
+      break;
+    case 5:
+      wildZone.Five();
+      break;
+    case 6:
+      wildZone.Six();
+      break;
+    case 7:
+      wildZone.Seven();
+      break;
+    case 8:
+      wildZone.Eight();
+      break;
+    case 9:
+      wildZone.Nine();
+      break;
+    case 10:
+      wildZone.Ten();
+      break;
+    case 11:
+      wildZone.Eleven();
+      break;
+    case 12:
+      wildZone.Twelve();
+      break;
+    case 13:
+      wildZone.Thirteen();
+      break;
+    case 14:
+      wildZone.Fourteen();
+      break;
+    case 15:
+      wildZone.Fifteen();
+      break;
+    case 16:
+      wildZone.Sixteen();
+      break;
+    case 17:
+      wildZone.Seventeen();
+      break;
+    case 18:
+      wildZone.Eighteen();
+      break;
+    case 19:
+      wildZone.Nineteen();
+      break;
+    case 20:
+      wildZone.Twenty();
+      break;
+    case 999:
+      simpleScript();
+      break;
+    default:
+      break;
+  }
 }
 
 // For writing simple scripts, use this function and call to it in the main. 
