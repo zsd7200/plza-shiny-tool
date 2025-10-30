68d3b3aa69745f46dc6f10b3be6edb82d9ac3c6a
Zack Dunham <dunhamz2016@gmail.com>
2025-10-30 15:18:33 -0400

    add a bunch of extra a presses to bench refresh to help with console timing variations

--- src/WaitTimes.cpp
+++ src/WaitTimes.cpp
@@ -5,5 +5,5 @@ unsigned long SWITCH_TWO_WAIT = 3500;
 unsigned long MAP_LOAD_WAIT = 100;
 unsigned long TEXT_WAIT = 600;
 unsigned long YES_WAIT = 50;
-unsigned long SWITCH_ONE_DAY_NIGHT_CYCLE_WAIT = 17000;
-unsigned long SWITCH_TWO_DAY_NIGHT_CYCLE_WAIT = 14000;
+unsigned long SWITCH_ONE_DAY_NIGHT_CYCLE_WAIT = 10000;
+unsigned long SWITCH_TWO_DAY_NIGHT_CYCLE_WAIT = 7000;
