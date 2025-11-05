1db6da83184a0a6938b1a2f6f8b5fc3bb29749cd
Zack Dunham <dunhamz2016@gmail.com>
2025-11-05 17:21:25 -0500

    untested preliminary pokemoncenter coords

--- src/locations/PokemonCenter.cpp
+++ src/locations/PokemonCenter.cpp
@@ -1,19 +1,35 @@
 #include "PokemonCenter.h"
 
+// untested
 void PokemonCenter::Magenta(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(150);
+  Down(700);
+  MapTravel(switchTwo);
 }
 
+// untested
 void PokemonCenter::Rogue(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(900);
+  Down(50);
+  MapTravel(switchTwo);
 }
 
+// untested
 void PokemonCenter::Hibernal(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(1200);
+  Down(450);
+  MapTravel(switchTwo);
 }
 
+// untested
 void PokemonCenter::Jaune(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(1750);
+  Down(550);
+  MapTravel(switchTwo);
 }
 
 void PokemonCenter::Centrico(boolean switchTwo = true) {
@@ -23,18 +39,34 @@ void PokemonCenter::Centrico(boolean switchTwo = true) {
   MapTravel(switchTwo);
 }
 
+// untested
 void PokemonCenter::MagentaPlaza(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(550);
+  Down(850);
+  MapTravel(switchTwo);
 }
 
+// untested
 void PokemonCenter::Vernal(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(900);
+  Down(1500);
+  MapTravel(switchTwo);
 }
 
+// untested
 void PokemonCenter::Bleu(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(450);
+  Down(1700);
+  MapTravel(switchTwo);
 }
 
+// untested
 void PokemonCenter::Vert(boolean switchTwo = true) {
-
+  Nothing(TOP_LEFT_DELAY);
+  Right(1400);
+  Down(1650);
+  MapTravel(switchTwo);
 }
